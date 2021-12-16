6ab8783e831837a0fcb06d3a4e22308a94a808c1
Victoria159 <v.s.bocha@gmail.com>
2021-12-16 18:07:11 +0300

    Changed 1.base-concepts/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,26 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+let d = b ** 2 - 4 * a * c;
+  let x1 = 0;
+  let x2 = 0;
+  let arr = [];
+  if (d == 0) {
+    x1 = (-b + Math.sqrt(d)) / 2 * a;
+    arr = [x1]
+  }
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / 2 * a;
+    x2 = (-b - Math.sqrt(d)) / 2 * a;
+    arr = [x1, x2];
+  }
+  if (d < 0) {
+    arr = [];
+  }
+  return arr;
 }
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
