3fc1c310ee7d3b193670b24804698159fbb22000
Victoria159 <v.s.bocha@gmail.com>
2021-12-23 21:15:28 +0300

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length && arr1.every(function(value, index) {
+    return value === arr2[index]
+  })
+  return result
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+ let result = arr.filter(function(number) {
+    return number > 0
+  }).filter(n => !(n % 3)).map(function(num) {
+    return num * 10
+  })
+    return result;
 }
