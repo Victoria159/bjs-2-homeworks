67f04b027efb4bcda37d11d9355b84b8c158667d
Victoria159 <v.s.bocha@gmail.com>
2022-01-25 14:47:13 +0300

    Added main.js

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,20 @@
-// тут вы можете вызывать функции из task.js
+const testCase = () => {
+    let alarmClock = new AlarmClock();
+    alarmClock.addClock('22:55', () => console.log ('New week!'), 1);
+    alarmClock.addClock('22:56', () => {
+        console.log('New week!!!');
+        alarmClock.printAlarms();
+        alarmClock.removeClock(2);
+        alarmClock.printAlarms();
+    }, 2);
+    alarmClock.addClock('22:57', () => {
+        console.log("Go sleep");
+        alarmClock.printAlarms();
+        alarmClock.stop();
+        alarmClock.printAlarms();
+    }, 3);
+    alarmClock.start();
+    alarmClock.printAlarms();
+}
+
+testCase();// тут вы можете вызывать функции из task.js
--- 7.async/task.js
+++ 7.async/task.js
@@ -58,13 +58,3 @@ class AlarmClock {
         return this.alarmCollection = [];
     }
 }
-// Образец:
-
-let PhoneClock = new AlarmClock();
-PhoneClock.addClock('22:55', () => console.log ('New week!'), 1);
-PhoneClock.addClock('22:56', () => console.log ('New week!!!'), 2);
-PhoneClock.removeClock(2);
-PhoneClock.addClock('22:57', () => console.log ('Go sleep'), 3);
-PhoneClock.start();
-PhoneClock.stop();
-PhoneClock.printAlarms();
